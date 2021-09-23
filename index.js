a98f904301ae101baed41fc87f219fb20fd92c74
Kamiya <kamitarg88@gmail.com>
2021-09-24 00:17:39 +0530

    feat: added edge cases for negative and zero value inputs

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ CalculateBtn.addEventListener("click", CalculateAreaOfRect);
 function CalculateAreaOfRect() {
     var L = Length.value;
     var B = Breadth.value;
+    if(L == 0 || B == 0){
+        Output.innerText = "Please Enter all required fields 🧐";
+    }
+    else if(L < 0 || B < 0){
+        Output.innerText = "Please Enter positive values only 😐";
+    }
+    else{
     var A = L*B;
     Output.innerText = `The area of Rectangle is ${A}cm².`
+}
 }
